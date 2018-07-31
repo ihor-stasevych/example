2c2e110590e9f5ca601ee92a116ac6a2ea62dc6d
root <kurch1992@gmail.com>
2018-07-31 15:56:08 +0000

    unit tests

---
+++ tests/Unit/Services/User/AccountSettings/GeneralInformationUpdateServiceTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 31.07.18
+ * Time: 12:19
+ */
+
+namespace App\Tests\Unit\Services\User\AccountSettings;
+
+
+class GeneralInformationUpdateServiceTest
+{
+
+}
