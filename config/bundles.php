efd4f85f3bed4bbd75fbb33abd246540a4541f50
knoxville <orine90@gmail.com>
2018-08-07 10:57:44 +0300

    added cors bundle

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
