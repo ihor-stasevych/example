ab47eaa41a55db782b7475ad56aae9da0ea1adee
Viacheslav <kurch1992@gmail.com>
2018-09-27 18:08:37 +0300

    re-creates the order

--- src/AddHash/AdminPanel/Domain/Store/Order/Command/StoreOrderCreateCommandInterface.php
+++ src/AddHash/AdminPanel/Domain/Store/Order/Command/StoreOrderCreateCommandInterface.php
@@ -2,13 +2,9 @@
 
 namespace App\AddHash\AdminPanel\Domain\Store\Order\Command;
 
-use App\AddHash\AdminPanel\Domain\User\User;
-
 interface StoreOrderCreateCommandInterface
 {
 	public function getProducts();
 
 	public function getItemsPriceTotal();
-
-	public function getUser() : User;
 }
