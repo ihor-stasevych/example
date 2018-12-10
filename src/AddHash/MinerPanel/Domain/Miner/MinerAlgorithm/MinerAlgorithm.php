2cafbbdf70615ccb02c2d8fa51fd7f9b6d9169ee
Viacheslav <kurch1992@gmail.com>
2018-12-10 12:27:14 +0200

    create entity MinerCoin add migration

--- src/AddHash/MinerPanel/Domain/Miner/MinerAlgorithm/MinerAlgorithm.php
+++ src/AddHash/MinerPanel/Domain/Miner/MinerAlgorithm/MinerAlgorithm.php
@@ -2,16 +2,23 @@
 
 namespace App\AddHash\MinerPanel\Domain\Miner\MinerAlgorithm;
 
+use Doctrine\ORM\PersistentCollection;
+use Doctrine\Common\Collections\ArrayCollection;
+
 class MinerAlgorithm
 {
     private $id;
 
     private $value;
 
+    private $coin;
+
     public function __construct(string $value, int $id = null)
     {
         $this->id = $id;
         $this->value = $value;
+        $this->coin = new ArrayCollection();
+
     }
 
     public function getId(): ?int
@@ -22,5 +29,13 @@ class MinerAlgorithm
     public function getValue(): string
     {
         return $this->value;
+    }
+
+    public function getCoin(): PersistentCollection
+    {
+        /** @var PersistentCollection $coin */
+        $coin = $this->coin;
+
+        return $coin;
     }
 }
---
+++ src/AddHash/MinerPanel/Domain/Miner/MinerAlgorithm/MinerCoin/MinerCoin.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\AddHash\MinerPanel\Domain\Miner\MinerAlgorithm\MinerCoin;
+
+use App\AddHash\MinerPanel\Domain\Miner\MinerAlgorithm\MinerAlgorithm;
+
+class MinerCoin
+{
+    private $id;
+
+    private $value;
+
+    private $algorithm;
+
+    public function __construct(string $value, MinerAlgorithm $algorithm, int $id = null)
+    {
+        $this->id = $id;
+        $this->value = $value;
+        $this->algorithm = $algorithm;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    public function getAlgorithm(): MinerAlgorithm
+    {
+        return $this->algorithm;
+    }
+}
