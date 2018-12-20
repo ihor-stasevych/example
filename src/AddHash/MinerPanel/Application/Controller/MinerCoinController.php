959c3ca3cf33ca864707725dd8b1b08f7cd55260
Viacheslav <kurch1992@gmail.com>
2018-12-20 17:51:18 +0200

    add route get miner coin

--- src/AddHash/MinerPanel/Application/Controller/MinerCoinController.php
+++ src/AddHash/MinerPanel/Application/Controller/MinerCoinController.php
@@ -5,20 +5,20 @@ namespace App\AddHash\MinerPanel\Application\Controller;
 use Swagger\Annotations as SWG;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use App\AddHash\System\GlobalContext\Common\BaseServiceController;
-use App\AddHash\MinerPanel\Application\Command\Miner\MinerPools\MinerPoolsGetCommand;
-use App\AddHash\MinerPanel\Domain\Miner\MinerPools\Services\MinerPoolsGetServiceInterface;
+use App\AddHash\MinerPanel\Application\Command\Miner\MinerAlgorithm\MinerCoin\MinerCoinGetCommand;
+use App\AddHash\MinerPanel\Domain\Miner\MinerAlgorithm\MinerCoin\Services\MinerCoinGetServiceInterface;
 
-class MinerPoolsController extends BaseServiceController
+class MinerCoinController extends BaseServiceController
 {
     private $getService;
 
-    public function __construct(MinerPoolsGetServiceInterface $getService)
+    public function __construct(MinerCoinGetServiceInterface $getService)
     {
         $this->getService = $getService;
     }
 
     /**
-     * Get miner pools
+     * Get miner coins
      *
      * @SWG\Parameter(
      *     name="id",
@@ -30,14 +30,14 @@ class MinerPoolsController extends BaseServiceController
      *
      * @SWG\Response(
      *     response=200,
-     *     description="Return miner pools",
+     *     description="Return miner coins",
      *     @SWG\Schema(
      *          type="array",
      *          @SWG\Items(
      *              type="object",
-     *              @SWG\Property(property="url", type="string"),
-     *              @SWG\Property(property="user", type="string"),
-     *              @SWG\Property(property="status", type="string"),
+     *              @SWG\Property(property="name", type="string"),
+     *              @SWG\Property(property="tag", type="string"),
+     *              @SWG\Property(property="income", type="number"),
      *          )
      *     ),
      * )
@@ -49,11 +49,11 @@ class MinerPoolsController extends BaseServiceController
      *
      * @param int $id
      * @return JsonResponse
-     * @SWG\Tag(name="MinerPanel_MinerPools")
+     * @SWG\Tag(name="MinerPanel_MinerCoin")
      */
     public function get(int $id)
     {
-        $command = new MinerPoolsGetCommand($id);
+        $command = new MinerCoinGetCommand($id);
 
         return $this->json(
             $this->getService->execute($command)
