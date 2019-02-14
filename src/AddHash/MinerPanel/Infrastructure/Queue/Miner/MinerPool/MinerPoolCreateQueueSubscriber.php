16c2f09ab86f0929a49bcc84037c64f4fd387039
knoxville <orine90@gmail.com>
2019-02-14 12:51:01 +0200

    log queue pools service

--- src/AddHash/MinerPanel/Infrastructure/Queue/Miner/MinerPool/MinerPoolCreateQueueSubscriber.php
+++ src/AddHash/MinerPanel/Infrastructure/Queue/Miner/MinerPool/MinerPoolCreateQueueSubscriber.php
@@ -5,6 +5,7 @@ namespace App\AddHash\MinerPanel\Infrastructure\Queue\Miner\MinerPool;
 use Interop\Queue\PsrContext;
 use Interop\Queue\PsrMessage;
 use Interop\Queue\PsrProcessor;
+use Psr\Log\LoggerInterface;
 use Enqueue\Consumption\QueueSubscriberInterface;
 use App\AddHash\MinerPanel\Application\Command\Miner\MinerPool\MinerPoolCreateCommand;
 use App\AddHash\MinerPanel\Domain\Miner\MinerPool\Services\MinerPoolCreateServiceInterface;
@@ -12,17 +13,27 @@ use App\AddHash\MinerPanel\Domain\Miner\MinerPool\Services\MinerPoolCreateServic
 class MinerPoolCreateQueueSubscriber implements PsrProcessor, QueueSubscriberInterface
 {
     private $minerPoolCreateService;
+    private $logger;
 
-    public function __construct(MinerPoolCreateServiceInterface $minerPoolCreateService)
+    public function __construct(
+    	MinerPoolCreateServiceInterface $minerPoolCreateService,
+		LoggerInterface $logger
+
+    )
     {
         $this->minerPoolCreateService = $minerPoolCreateService;
+        $this->logger = $logger;
     }
 
     public function process(PsrMessage $message, PsrContext $context)
     {
+
+    	$this->logger->debug('PoolCreateQueue income message: ' . var_export($message));
+
         $data = json_decode($message->getBody(), true);
 
         if (!$data) {
+	        $this->logger->error('PoolCreateQueue error message: ' . var_export($message));
         	echo 'Incorrect json format message: '. var_dump($message->getBody());
         	return self::REJECT;
         }
